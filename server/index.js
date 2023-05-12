c543a6e71f0ad9825a08a44392cfd0a712683836
gorguero <jcaguerodev@gmail.com>
2023-05-11 21:01:02 -0300

    Inicio configuración del servidor

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log("it´s funny")
+const express = require('express'); 
+
+const app = express(); //Creación de nuestro servidor
+
+app.get('/', (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'OK'
+    })
+    
+})
+
+app.listen( 5000, () => {
+    console.log('Conectado al puerto 5000')
+} )
