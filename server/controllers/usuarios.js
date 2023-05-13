4b15a4b975ed6b80ab5d22e183a258bff74af109
gorguero <jcaguerodev@gmail.com>
2023-05-13 18:41:31 -0300

    Validaciones si existe correo e ID

--- server/controllers/usuarios.js
+++ server/controllers/usuarios.js
@@ -53,10 +53,30 @@ const actualizarUsuario = async(req, res = response) => {
 
     try {
         
+        const existeUsuarioDB = await Usuario.findById( uid );
 
+        if( !existeUsuarioDB ){
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe usuario con ese ID'
+            })
+        }
 
         //Actualizacion de los datos
         const { password, email, ...campos } = req.body;
+
+        if(existeUsuarioDB.email != email){
+
+            const existeEmail = await Usuario.findOne({ email });
+
+            if(existeEmail){
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Ya existe un usuario con ese email'
+                })
+            }
+
+        }
         
         campos.email = email;
         const usuarioActualizado = await Usuario.findByIdAndUpdate( uid, campos, { new: true } );
