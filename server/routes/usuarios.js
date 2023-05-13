89eccef10a4127824cd3098f59f3104df77c6c8c
gorguero <jcaguerodev@gmail.com>
2023-05-13 17:42:12 -0300

    Validaciones con Middlawares

--- server/routes/usuarios.js
+++ server/routes/usuarios.js
@@ -1,8 +1,17 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { getUsuarios, crearUsuario, actualizarUsuario, eliminarUsuario } = require('../controllers/usuarios');
+const { validarCampos } = require('../middlewares/validaciones');
 const router = Router();
 
-router.post('/', crearUsuario);
+router.post('/', 
+    [
+        check('nombre','El nombre es obligatorio').not().isEmpty(),
+        check('email','El email es obligatorio').not().isEmpty(),
+        check('password','El password es obligatorio').not().isEmpty(),
+        validarCampos
+    ]
+    ,crearUsuario);
 
 router.get('/', getUsuarios);
 
