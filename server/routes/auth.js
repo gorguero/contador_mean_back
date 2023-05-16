19bd41caf165dad4a889d193a83c5b7b27089c07
gorguero <jcaguerodev@gmail.com>
2023-05-16 18:16:00 -0300

    validacion login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 const { login } = require('../controllers/auth');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validaciones');
 
 const router = Router();
 
 router.post('/',
     [
-
+        check('email', 'El correo es obligatorio').isEmail(),
+        check('password', 'El password es obligatorio').not().isEmpty(),
+        validarCampos
     ], 
     login
 );
