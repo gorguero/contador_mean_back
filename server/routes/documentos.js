ce3a891abcd1264da4e6f884d164ac0d85466e2f
gorguero <jcaguerodev@gmail.com>
2023-05-20 16:22:01 -0300

    Creando rutas

---
+++ server/routes/documentos.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const {getDocumentos, getDocumentosByID, getMisDocumentos, crearDocumento, editarDocumento, actualizarDocumento, eliminarDocumento} = require('../controllers/documentos');
+const { validarCampos } = require('../middlewares/validaciones');
+const { validarJWT } = require('../middlewares/validarjwt');
+
+const router = Router();
+
+router.get('/', getDocumentos);
+router.get('/editar-documentos/:id', getDocumentosByID);
+router.get('/mis-documentos/:id', getMisDocumentos);
+router.post('/', crearDocumento);
+router.put('/:id', editarDocumento);
+router.put('/editar-documento/:id', actualizarDocumento);
+router.delete('/:id', eliminarDocumento);
+
+module.exports = router;
