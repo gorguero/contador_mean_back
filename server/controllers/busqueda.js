b1cbc5cc0f164aeade3c17f23ab71294e9d06bfa
gorguero <jcaguerodev@gmail.com>
2023-05-30 08:27:47 -0300

    Busqueda por colección

--- server/controllers/busqueda.js
+++ server/controllers/busqueda.js
@@ -24,10 +24,33 @@ const getBuscar = async(req, res=response) => {
 
 const getDocumentos = async(req, res=response) => {
 
+    const tabla = req.params.tabla;
+    const buscar = req.params.buscar;
+    const regExp = new RegExp(buscar, 'i');
+
+    let data = [];
+
+    switch(tabla){
+        
+        case 'documentos':
+            data = await Documento.find({nombre: regExp});
+        break;
+
+        case 'usuarios':
+            data = await Usuario.find({nombre: regExp});
+        break;
+
+        default:
+            return res.status(400).json({
+                ok: true,
+                msg: 'La tabla debe ser de documentos o usuarios'
+            });
+    }
+
     res.json({
         ok: true,
-        msg: 'ok en buscar documentos'
-    })
+        resultado: data
+    });
 
 }
 
