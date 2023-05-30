d1653758d4ded897af057253b3f8b519aaf7e04c
gorguero <jcaguerodev@gmail.com>
2023-05-30 10:59:09 -0300

    Funcion borrar pdf

--- server/helpers/actualizar-PDF.js
+++ server/helpers/actualizar-PDF.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+
+const borrarPDF = (path) => {
+    if( fs.existsSync(path) ){
+        fs.unlinkSync(path);
+    }
+}
+
 const actualizarPDF = () => {
     console.log('estoy actualizando el pdf')
 }
