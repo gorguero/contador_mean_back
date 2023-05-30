0373d1e62f759e4cb9d6544e07a13f827b7112d3
gorguero <jcaguerodev@gmail.com>
2023-05-30 09:17:19 -0300

    Validando la coleccion

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -1,6 +1,20 @@
 const {response} = require('express');
 
 const fileUpload = (req, res=response) => {
+
+    const coleccion = req.params.coleccion;
+    const id = req.params.id;
+
+    const validarColeccion = ['documentos', 'usuarios'];
+
+    //validamos la coleccion
+    if( !validarColeccion.includes(coleccion) ){
+        return res.status(400).json({
+            ok: false,
+            msg: 'No existe la coleccion'
+        })
+    }
+
     res.json({
         ok: true,
         msg: 'Subiendo archivo'
